d152992cc305b55b324be7408f0c265b5289eb23
AndyLeung <407833635@qq.com>
2023-08-14 21:01:39 +0800

    update variables

--- ch03_variables/src/main.rs
+++ ch03_variables/src/main.rs
@@ -1,25 +1,56 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
-
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-
-    let y = 5;
-    let y = y + 1;
+    //let mut
     {
-        let y = y * 2;
-        println!("The value of x in the inner scope is: {y}");
+        let mut x = 5;
+        println!("The value of x is: {x}");
+        x = 6;
+        println!("The value of x is: {x}");
     }
-    println!("The value of x is: {x}");
 
-    let spaces = "    ";
-    let spaces = spaces.len();
-    println!("{spaces}");
+    //const
+    {
+        const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    }
 
-    let xx = 110.15;
-    let yy: f32 = 100.0;
-    let zz = xx - yy;
-    println!("{zz}");
+    //let
+    {
+        let y = 5;
+        let y = y + 1;
+        {
+            let y = y * 2;
+            println!("The value of y in the inner scope is: {y}");
+        }
+        println!("The value of y is: {y}");
+
+        let spaces = "    ";
+        let spaces = spaces.len();
+        println!("{spaces}");
+    }
+
+    //f64,f32
+    {
+        let xx = 110.15;
+        let yy: f32 = 100.0;
+        let zz = xx - yy;
+        println!("{zz}");
+    }
+
+    //tuple
+    {
+        let tup: (i32, f64, u8) = (500, 6.4, 1);
+        let tup2 = (500, 6.4, 1);
+        let (x, y, z) = tup; //解构(destructuring)
+        println!("The value of y is: {y}");
+        let thrid = tup.2;
+        let tup3 = ();
+        println!("The value of z is: {thrid}");
+    }
+
+    //array
+    {
+        let a = [1, 2, 3, 4, 5];
+        let b: [i32; 5] = [1, 2, 3, 4, 5];
+        let c = [3; 5]; //[3,3,3,3,3]
+        let first = a[0];
+    }
 }
