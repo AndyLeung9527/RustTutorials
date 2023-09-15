d2ddd782f40bac376f4f2eeff2e561cf0de832e4
AndyLeung <407833635@qq.com>
2023-09-15 18:02:55 +0800

    Add ch12_minigrep

---
+++ ch12_minigrep/src/main.rs
@@ -0,0 +1,32 @@
+use minigrep::Config;
+use std::env; //注意std::env::args在其任何参数包含无效Unicode字符时会panic
+use std::process;
+
+//分别执行下列指令(powershell)，以设置环境变量和执行
+//$Env:IGNORE_CASE=1;
+//cargo run -- to E:\LWB_Projects\RustTutorial\ch12_minigrep\poem.txt
+//执行下列指令(powershell)移除环境变量
+//Remove-Item Env:IGNORE_CASE
+//分别执行下列指令，标准输出会重定向到output.txt，错误输出会打印到标准错误流
+//cargo run > output.txt
+//cargo run -- to E:\LWB_Projects\RustTutorial\ch12_minigrep\poem.txt > output.txt
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        //println!("Problem parsing arguments:{err}");
+        eprintln!("Problem parsing arguments:{err}"); //打印到标准错误流
+        process::exit(1);
+    });
+
+    println!(
+        "Searching for {} in file {}",
+        config.query, config.file_path
+    );
+
+    if let Err(e) = minigrep::run(config) {
+        //println!("Application error:{e}");
+        eprintln!("Application error:{e}"); //打印到标准错误流
+        process::exit(1);
+    }
+}
